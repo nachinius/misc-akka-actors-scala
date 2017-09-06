e4af20878d7d1f96799c621bc1edeb7312f55ebd
nachinius <ignacio.peixoto@gmail.com>
2017-09-06 15:56:32 -0300

    The actor compiler returns futures

--- src/main/scala/com/nachinius/free/KeyValue.scala
+++ src/main/scala/com/nachinius/free/KeyValue.scala
@@ -1,23 +1,15 @@
 package com.nachinius.free
 
-import akka.Done
-import akka.actor.{Actor, ActorRef, ActorSystem, Props}
-import akka.pattern.ask
-
-import scala.concurrent.duration._
+import akka.actor.{Actor, ActorSystem, Props}
 import akka.util.Timeout
+import cats.data.State
 import cats.free.Free
 import cats.free.Free.liftF
-import cats.data.State
-import cats.arrow.FunctionK
 import cats.{Id, ~>}
 
 import scala.collection.mutable
+import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
-
-class KeyValue {
-
-}
 
 
 object KeyValue {
@@ -30,16 +22,11 @@ object KeyValue {
 
   case class Delete(key: String) extends KVStoreADT[Unit]
 
-
   type KVStore[A] = Free[KVStoreADT, A]
 
   // Put returns nothing (i.e. Unit).
   def put[T](key: String, value: T): KVStore[Unit] =
     liftF[KVStoreADT, Unit](Put[T](key, value))
-
-  // Get returns a T value.
-  def get[T](key: String): KVStore[Option[T]] =
-    liftF[KVStoreADT, Option[T]](Get[T](key))
 
   // Delete returns nothing (i.e. Unit).
   def delete(key: String): KVStore[Unit] =
@@ -52,6 +39,9 @@ object KeyValue {
       _ <- vMaybe.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
     } yield ()
 
+  // Get returns a T value.
+  def get[T](key: String): KVStore[Option[T]] =
+    liftF[KVStoreADT, Option[T]](Get[T](key))
 
   def program: KVStore[Option[Int]] =
     for {
@@ -65,59 +55,71 @@ object KeyValue {
   type KVStoreState[A] = State[Map[String, Any], A]
   val pureCompiler: KVStoreADT ~> KVStoreState = new (KVStoreADT ~> KVStoreState) {
     def apply[A](fa: KVStoreADT[A]): KVStoreState[A] =
-      fa match {
-        case Put(key, value) => State.modify(_.updated(key, value))
-        case Get(key) =>
-          State.inspect(_.get(key).map(_.asInstanceOf[A]))
-        case Delete(key) => State.modify(_ - key)
-      }
+    fa match {
+      case Put(key, value) => State.modify(_.updated(key, value))
+      case Get(key) =>
+        State.inspect(_.get(key).map(_.asInstanceOf[A]))
+      case Delete(key) => State.modify(_ - key)
+    }
+  }
+
+  type KVStoreActor[A] = State[ActorState, A]
+  val actorCompiler: KVStoreADT ~> KVStoreActor = new (KVStoreADT ~> KVStoreActor) {
+    override def apply[A](fa: KVStoreADT[A]): KVStoreActor[A] = fa match {
+      case Put(key, value) =>
+        State.modify({ s: ActorState =>
+          s.tell((key, Some(value)))
+          s
+        })
+      case Get(key) =>
+        val f = (state: ActorState) => (state, {
+          val a = Await.result(state.ask2(key), 1 second).asInstanceOf[A]
+          Some(a)
+        })
+        State(f)
+      case Delete(key) =>
+        State.modify({ s: ActorState =>
+          s.tell((key, None))
+          s
+        })
+    }
   }
 
   // the program will crash if a key is not found,
   // or if a type is incorrectly specified.
   def impureCompiler: KVStoreADT ~> Id = new (KVStoreADT ~> Id) {
-
     // a very simple (and imprecise) key-value store
     val kvs = mutable.Map.empty[String, Any]
 
     def apply[A](fa: KVStoreADT[A]): Id[A] =
       fa match {
         case Put(key, value) =>
-
           kvs(key) = value
           ()
         case Get(key) =>
-
           kvs.get(key).map(_.asInstanceOf[A])
         case Delete(key) =>
-
           kvs.remove(key)
           ()
       }
   }
 
   def impureFutureCompiler(implicit ec: ExecutionContext): KVStoreADT ~> Future = new (KVStoreADT ~> Future) {
-
-    import cats.instances.future._
-
     // a very simple (and imprecise) key-value store
     val kvs = mutable.Map.empty[String, Any]
 
     def apply[A](fa: KVStoreADT[A]): Future[A] =
       fa match {
         case Put(key, value) =>
-
           kvs(key) = value
           Future {
             ()
           }
         case Get(key) =>
-
           Future {
             (kvs.get(key).map(_.asInstanceOf[A]))
           }
         case Delete(key) =>
-
           kvs.remove(key)
           Future {
             ()
@@ -125,35 +127,31 @@ object KeyValue {
       }
   }
 
+  def actorFutureCompiler(actorFutureState: ActorFutureState): KVStoreADT ~> Future = new (KVStoreADT ~> Future) {
+    implicit val context = actorFutureState.system.dispatcher
+    override def apply[A](fa: KVStoreADT[A]): Future[A] = fa match {
+      case Put(key, value) =>
+        actorFutureState.ask2((key, Some(value))).map(_ => ())
+
+      case Get(key) =>
+        actorFutureState.ask2(key).asInstanceOf[Future[A]]
+
+      case Delete(key) =>
+        actorFutureState.ask2((key, None)).map(_ => ())
+    }
+  }
+
+
+
 
   case class ActorState(system: ActorSystem) {
-
-    class KeyValueActor extends Actor {
-      var state = Map[String, Any]()
-
-      override def receive: Receive = {
-        case (k: String) =>
-
-          sender() ! state(k)
-        case (k: String, None) =>
-          state = state - k
-        case (k: String, Some(v)) =>
-
-          state = state.updated(k, v)
-
-
-      }
-    }
+    //    val system = ActorSystem()
+    val ref = system.actorOf(Props(new KeyValueActor))
 
     import akka.pattern.ask
 
-    //    val system = ActorSystem()
-    val ref = system.actorOf(Props(new KeyValueActor))
-    implicit val timeout = Timeout(5 seconds) // needed for `?` below
-
     def terminate() = system.terminate()
-
-    import system.dispatcher
+    implicit val timeout = Timeout(5 seconds) // needed for `?` below
 
     def tell(x: Any): Unit =
       ref ! x
@@ -162,9 +160,36 @@ object KeyValue {
       ask(ref, x)
     }
 
+    class KeyValueActor extends Actor {
+      var state = Map[String, Any]()
+
+      override def receive: Receive = {
+        case (k: String) =>
+          sender() ! state(k)
+        case (k: String, None) =>
+          state = state - k
+        case (k: String, Some(v)) =>
+          state = state.updated(k, v)
+      }
+    }
+
   }
 
   case class ActorFutureState(system: ActorSystem) {
+    //    val system = ActorSystem()
+    val ref = system.actorOf(Props(new KeyValueActor))
+
+    def terminate() = system.terminate()
+
+    import akka.pattern.ask
+
+    def tell(x: Any): Unit =
+      ref ! x
+    implicit val timeout = Timeout(5 seconds) // needed for `?` below
+
+    def ask2[A](x: A): Future[Any] = {
+      ask(ref, x)
+    }
 
     case class Done()
 
@@ -174,7 +199,7 @@ object KeyValue {
       override def receive: Receive = {
         case (k: String) =>
 
-          sender() ! state(k)
+          sender() ! state.get(k)
         case (k: String, None) =>
           state = state - k
           sender() ! Done
@@ -186,77 +211,7 @@ object KeyValue {
       }
     }
 
-    import akka.pattern.ask
-
-    //    val system = ActorSystem()
-    val ref = system.actorOf(Props(new KeyValueActor))
-    implicit val timeout = Timeout(5 seconds) // needed for `?` below
-
-    def terminate() = system.terminate()
-
-    import system.dispatcher
-
-    def tell(x: Any): Unit =
-      ref ! x
-
-    def ask2(x: Any): Future[Any] = {
-      ask(ref, x)
-    }
-
   }
 
-  type KVStoreActor[A] = State[ActorState, A]
-  val actorCompiler: KVStoreADT ~> KVStoreActor = new (KVStoreADT ~> KVStoreActor) {
 
-
-    override def apply[A](fa: KVStoreADT[A]): KVStoreActor[A] = fa match {
-      case Put(key, value) =>
-
-
-        //        kvs(key) = value
-        State.modify({ s: ActorState =>
-
-          s.tell((key, Some(value)))
-          s
-        })
-
-      case Get(key) =>
-
-        //        kvs.get(key).map(_.asInstanceOf[A])
-        val f = (state: ActorState) => (state, {
-          val a = Await.result(state.ask2(key), 1 second).asInstanceOf[A]
-          Some(a)
-        })
-        State(f)
-      case Delete(key) =>
-
-        State.modify({ s: ActorState =>
-          s.tell((key, None))
-          s
-        })
-      //        kvs.remove(key)
-
-    }
-  }
-
-//  type FutureKVStoreActor[A] = Future[State[ActorState, A]]
-//  val actorFutureCompiler: KVStoreADT ~> FutureKVStoreActor = new (KVStoreADT ~> FutureKVStoreActor) {
-//
-//
-//    override def apply[A](fa: KVStoreADT[A]): FutureKVStoreActor[A] = fa match {
-//      case Put(key, value) =>
-//
-//
-//
-//      case Get(key) =>
-//
-//
-//      case Delete(key) =>
-//
-//
-//
-//    }
-//  }
-
-  
 }
--- src/test/scala/com/nachinius/free/KeyValueTest.scala
+++ src/test/scala/com/nachinius/free/KeyValueTest.scala
@@ -2,8 +2,11 @@ package com.nachinius.free
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
-import com.nachinius.free.KeyValue.{ActorState, actorCompiler, program}
+import com.nachinius.free.KeyValue.{ActorState, actorCompiler, program, _}
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers, WordSpecLike}
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.DurationLong
 
 class KeyValueTest  extends TestKit(ActorSystem("MySpec")) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
@@ -25,8 +28,20 @@ class KeyValueTest  extends TestKit(ActorSystem("MySpec")) with ImplicitSender
       import system.dispatcher
       import cats.instances.future._
 
-      val p=program.foldMap(KeyValue.impureFutureCompiler)
-      p.map(x=>assert(x==Some(14)))
+      val p: Future[Option[Int]] =program.foldMap(KeyValue.impureFutureCompiler)
+      assert(Await.result(p,1 second)==Some(14))
+    }
+  }
+
+  "actor future compiler" must{
+    "work with futures" in {
+      import system.dispatcher
+      import cats.instances.future._
+
+      val compiler = actorFutureCompiler(ActorFutureState(system))
+      val p=program.foldMap(compiler)
+      assert(Await.result(p,1 second)==Some(14))
+
     }
   }
 }
